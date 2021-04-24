a11edecc9058d3b3479157946dc8596d4d18f945
Yusuf "Rain_menn" Başaran <80125850+rain-menn@users.noreply.github.com>
2021-04-24 14:56:50 +0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+function uzerinegelince()
+{
+  document.getElementById("mesaj").innerHTML="Türkiye'ye Başarılar...";
+  document.getElementById("mesaj").style.color="#f00";
+}
+function uzerindenayrilince()
+{
+  document.getElementById("mesaj").innerHTML="Euro 2021'da";
+  document.getElementById("mesaj").style.color="#000";
+}
